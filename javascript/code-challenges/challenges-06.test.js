88762c95c39260ffc06cad06fb77c3e9884fcae1
Caleb Hemphill <calebhemp@gmail.com>
2023-12-05 00:04:08 -0500

    Solve challenge 5

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -115,7 +115,10 @@ let starWarsData = [{
 }];
 
 const returnNames = (arr) => {
-  // Solution code here...
+  return arr.reduce( (names, character) => {
+    names.push(character.name);
+    return names;
+  }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
