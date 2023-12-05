ab8f220beebe7cf03eecc6b129a9d55e1ab7c184
Caleb Hemphill <calebhemp@gmail.com>
2023-12-04 23:54:24 -0500

    Solve challenge 4

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -52,7 +52,10 @@ Note: You may not use the array's built-in length property.
 ------------------------------------------------------------------------------------------------ */
 
 const countNumberOfElements = (arr) => {
-  // Solution code here...
+  return arr.reduce( (count) => {
+    count++;
+    return count;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
