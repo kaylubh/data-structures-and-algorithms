c55a958ef440b25442a7d0451cd28c1854e8124f
Caleb Hemphill <calebhemp@gmail.com>
2023-12-07 01:18:31 -0500

    Solve challenge 4

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -64,7 +64,10 @@ For example, if the input is 'Welcome', the output will be:
 
 const howMuchPencil = (str) => {
   let result = [];
-  // Solution code here...
+  for (let i = 0; i <= str.length; i++) {
+    const pencil = str.slice(i);
+    result.push(pencil);
+  }
   return result;
 };
 
