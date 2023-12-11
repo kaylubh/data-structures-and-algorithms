081d144c94b734f91e11b06d54c5e4e97657f8b2
Caleb Hemphill <calebhemp@gmail.com>
2023-12-10 23:20:32 -0500

    Solve challenge 8

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -185,7 +185,13 @@ For example, the following input returns a product of 720: [[1,2], [3,4], [5,6]]
 ------------------------------------------------------------------------------------------------ */
 
 const calculateProduct = (numbers) => {
-  // Solution code here...
+  let product = 1;
+  numbers.forEach(numberSet => {
+    for (let i = 0; i < numberSet.length; i++) {
+      product *= numberSet[i];
+    }
+  });
+  return product;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -328,7 +334,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should multiply all the numbers together', () => {
     expect(calculateProduct([[1, 2], [3, 4], [5, 6]])).toStrictEqual(720);
   });
