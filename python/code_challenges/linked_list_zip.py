013c3abff946c3a4d281ba5b743a71ef45dee5d6
Caleb Hemphill <calebhemp@gmail.com>
2024-01-19 00:25:39 -0500

    Create zip_lists function

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,2 +1,22 @@
+from data_structures.linked_list import LinkedList
+
 def zip_lists(a, b):
-    pass
+    """
+
+    """
+
+    zipped_list = LinkedList()
+
+    list_a_node = a.head
+    list_b_node = b.head
+
+    while list_a_node or list_b_node:
+        if list_a_node:
+            zipped_list.append(list_a_node)
+            list_a_node = list_a_node.next
+
+        if list_b_node:
+            zipped_list.append(list_b_node)
+            list_b_node = list_b_node.next
+
+    return zipped_list
