32a36931d875c82b363bb8a4c4dc23b7310c26d3
Caleb Hemphill <calebhemp@gmail.com>
2024-01-23 22:36:57 -0500

    Add docstrings

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -3,7 +3,7 @@ from data_structures.queue import Queue
 
 class AnimalShelter:
     """
-
+    Instantiates an object representation of an animal shelter which instantiates two Queue classes to represent shelters for cats and dogs. Follows FIFO principles. Intended to be used with the Cat and Dog classes.
     """
 
     def __init__(self):
@@ -12,7 +12,7 @@ class AnimalShelter:
 
     def enqueue(self, animal):
         """
-
+        Checks is the animal object being enqueued is a cat or dog and adds it to the appropriate queue.
         """
 
         if animal.species == "Cat":
@@ -23,7 +23,7 @@ class AnimalShelter:
 
     def dequeue(self, preference):
         """
-
+        Given a preference of "cat" or "dog", returns a cat or dog object from the appropriate queue according to FIFO principles.
         """
 
         dequeued_animal = None
@@ -38,7 +38,11 @@ class AnimalShelter:
 
 class Animal:
     """
+    Instantiates an object representation of an animal. Intended to be used as a base class for the Dog and Cat classes.
 
+    Attributes:
+    species (string): __name__ attribute of subclass which instantiates this instance of animal
+    name (string): name of the animal
     """
 
     def __init__(self, name):
@@ -50,7 +54,7 @@ class Animal:
 
 class Dog(Animal):
     """
-
+    Instantiates an object representation of a dog. Extends the Animal class.
     """
 
     def __init__(self, name = None):
@@ -58,7 +62,7 @@ class Dog(Animal):
 
 class Cat(Animal):
     """
-
+    Instantiates an object representation of a cat. Extends the Animal class.
     """
 
     def __init__(self, name = None):
