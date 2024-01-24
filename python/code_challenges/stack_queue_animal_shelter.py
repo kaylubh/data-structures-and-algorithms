a9508b88f8856f4aea1c964def2cd4994d6af5d2
Caleb Hemphill <calebhemp@gmail.com>
2024-01-23 22:13:58 -0500

    Create AnimalShelter class

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,8 +2,39 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
+    """
 
+    """
+
+    def __init__(self):
+        self.cats_shelter = Queue()
+        self.dogs_shelter = Queue()
+
+    def enqueue(self, animal):
+        """
+
+        """
+
+        if animal.species is "Cat":
+            self.cats_shelter.enqueue(animal)
+
+        if animal.species is "Dog":
+            self.dogs_shelter.enqueue(animal)
+
+    def dequeue(self, preference):
+        """
+
+        """
+
+        dequeued_animal = None
+
+        if preference is "cat":
+            dequeued_animal = self.cats_shelter.dequeue()
+
+        if preference is "dog":
+            dequeued_animal = self.dogs_shelter.dequeue()
+
+        return dequeued_animal
 
 class Animal:
     """
