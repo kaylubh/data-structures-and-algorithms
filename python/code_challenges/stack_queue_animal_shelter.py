92c6141ac2d4d285be03ecaf0f3046c19a86dd3c
Caleb Hemphill <calebhemp@gmail.com>
2024-01-23 22:04:10 -0500

    Create animal classes

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -5,9 +5,30 @@ class AnimalShelter:
     pass
 
 
-class Dog:
-    pass
+class Animal:
+    """
 
+    """
 
-class Cat:
-    pass
+    def __init__(self, name):
+        self.species = self.__class__.__name__
+        self.name = name
+
+    def __str__(self):
+        return f"Species: {self.species}, Name: {self.name}"
+
+class Dog(Animal):
+    """
+
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
+
+class Cat(Animal):
+    """
+
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
