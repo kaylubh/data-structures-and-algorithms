a16c6255c58743f2d98ee75545b4805adf8f33f7
Caleb Hemphill <calebhemp@gmail.com>
2024-01-29 02:45:30 -0500

    Create BinarySearchClass

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -6,10 +6,51 @@ class BinarySearchTree(BinaryTree):
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def add(self, value):
+        """
 
-    def some_method(self):
-        # method body here
-        pass
+        """
+
+        new_node = Node(value)
+
+        # if tree is empty then new node is root
+        if self.root is None:
+            self.root = new_node
+            return
+
+        # otherwise find the correct spot to add the new node at
+        def bst_traverse_add(root_node, new_node):
+            """
+
+            """
+
+            if new_node.value < root_node.value:
+
+                if root_node.left is None:
+                    root_node.left = new_node
+                else:
+                    bst_traverse_add(root_node.left, new_node)
+
+            elif new_node.value >= root_node.value:
+
+                if root_node.right is None:
+                    root_node.right = new_node
+                else:
+                    bst_traverse_add(root_node.right, new_node)
+
+        bst_traverse_add(self.root, new_node)
+
+    def contains(self, target_value):
+        """
+
+        """
+
+        node_to_check = self.root
+
+        while node_to_check is not None:
+            if target_value < node_to_check.value:
+                node_to_check = node_to_check.left
+            elif target_value > node_to_check.value:
+                node_to_check = node_to_check.right
+
+        return node_to_check.value == target_value
