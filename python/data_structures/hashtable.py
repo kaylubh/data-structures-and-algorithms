ac42751f3098e07709de392bd768fdd406942a68
Caleb Hemphill <calebhemp@gmail.com>
2024-02-17 13:00:40 -0500

    Add hash, set, and get methods

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -1,12 +1,96 @@
+from data_structures.linked_list import LinkedList
+
+
 class Hashtable:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, size=1024):
+        self.size = size
+        self._buckets = size * [None]
 
-    def some_method(self):
-        # method body here
-        pass
+    def set(self, key, value):
+        """
+
+        """
+
+        drop = [key, value]
+        index = self._hash(key)
+        bucket = self._buckets[index]
+
+        # check for existing key or collision
+        if bucket:
+
+            # check for existing key
+            current = bucket.head
+            while current:
+                current_drop = current.value
+                if current_drop[0] == key:
+                    current_drop[1] = value
+                    return
+                current = current.next
+
+            # if collision insert to list
+            bucket.insert(drop)
+
+        # bucket is empty
+        else:
+
+            bucket = LinkedList()
+            bucket.insert(drop)
+            self._buckets[index] = bucket
+
+    def get(self, key):
+        """
+
+        """
+
+        index = self._hash(key)
+        bucket = self._buckets[index]
+
+        # check if anything at index
+        if bucket is None:
+            return None
+
+        # check for key
+        current = bucket.head
+        while current:
+            current_drop = current.value
+            if current_drop[0] == key:
+                return current_drop[1]
+            current = current.next
+
+        # if collision but no matching key
+        return None
+
+    def has(self):
+        """
+
+        """
+
+    def keys(self):
+        """
+
+        """
+
+    def values(self):
+        """
+
+        """
+
+    def _hash(self, key):
+        """
+
+        """
+
+        if type(key) != str:
+            raise TypeError('key must be a string')
+
+        index = 0
+        for char in key:
+            index += ord(char)
+        index *= 461
+        index = index % self.size
+
+        return index
--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -5,15 +5,55 @@ from data_structures.hashtable import Hashtable
 def test_exists():
     assert Hashtable
 
+### test hash method ###
+# @pytest.mark.skip("TODO")
+def test_hash_range():
+    hashtable = Hashtable()
+    actual = hashtable._hash('test')
+    assert 0 <= actual < hashtable.size
 
-@pytest.mark.skip("TODO")
-def test_get_apple():
+# @pytest.mark.skip("TODO")
+def test_hash_same():
+    hashtable = Hashtable()
+    actual = hashtable._hash('test')
+    actual2 = hashtable._hash('test')
+    assert actual == actual2
+
+### test set and get methods ###
+# @pytest.mark.skip("TODO")
+def test_set_one_get_one():
     hashtable = Hashtable()
     hashtable.set("apple", "Used for apple sauce")
     actual = hashtable.get("apple")
     expected = "Used for apple sauce"
     assert actual == expected
 
+# @pytest.mark.skip("TODO")
+def test_set_two_get_one():
+    hashtable = Hashtable()
+    hashtable.set("apple", "Used for apple sauce")
+    hashtable.set('orange', 'used for orange juice')
+    actual = hashtable.get("apple")
+    expected = "Used for apple sauce"
+    assert actual == expected
+
+# @pytest.mark.skip("TODO")
+def test_key_not_exist():
+    hashtable = Hashtable()
+    hashtable.set("apple", "Used for apple sauce")
+    hashtable.set('orange', 'used for orange juice')
+    actual = hashtable.get('banana')
+    expected = None
+    assert actual == expected
+
+# @pytest.mark.skip("TODO")
+def test_bucket_collision():
+    hashtable = Hashtable()
+    hashtable.set('dear', 'the beginning of a letter')
+    hashtable.set('read', 'the past tense of read')
+    actual = hashtable.get('dear')
+    expected = 'the beginning of a letter'
+    assert actual == expected
 
 @pytest.mark.skip("TODO")
 def test_internals():
