91174201e60381c85a54574690db2749b85c2206
Caleb Hemphill <calebhemp@gmail.com>
2024-02-21 23:53:28 -0500

    Add solution code

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,23 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(hashmap_a, hashmap_b):
+    """
+
+    """
+
+    joined_table = []
+
+    for key, value in hashmap_a.items():
+        joined_record = []
+        joined_record.append(key)
+        joined_record.append(value)
+
+        if key in hashmap_b:
+            joined_record.append(hashmap_b[key])
+        else:
+            joined_record.append('NONE')
+
+        joined_table.append(joined_record)
+
+    return joined_table
